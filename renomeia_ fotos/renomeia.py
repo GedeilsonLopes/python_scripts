056b9f3717a69abee4b7da0d2166ce8b2ff63697
Gedeilson <gedeilsonlps@gmail.com>
2020-12-20 13:47:05 -0300

    renomeia fotos dentro de um diretório

--- renomeia_ fotos/renomeia.py
+++ renomeia_ fotos/renomeia.py
@@ -2,12 +2,12 @@ import os
 
 listaFotos = []
 
-os.chdir("renomeia_ fotos\img")
+os.chdir("renomeia_ fotos\img") #Seleciona o diretório
 
-for foto in os.listdir():
+for foto in os.listdir():       #Adiciona os nomes dos arquivos a uma lista
     listaFotos.append(foto)
 
 for i in range(len(listaFotos)):
-    os.rename(listaFotos[i],f'Lianna_{str(i+1)}')
+    os.rename(listaFotos[i],f'Lianna_{str(i+1)}')   # Modifica os nome dos arquivos
 
 print(listaFotos)
